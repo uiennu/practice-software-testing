71986e5b00de7e06d0febda43111d40708acb7e8
Nu <nhauyenkg05@gmail.com>
2025-07-30 01:14:08 +0700

    Update Test Suites

--- Demo_CI_CD/Test Suites/Test suites 1.ts
+++ Demo_CI_CD/Test Suites/Test suites 1.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>ca3d3139-4b99-485a-8403-a32cf983ec32</testSuiteGuid>
+   <testCaseLink>
+      <guid>a200269a-6550-450b-a46b-4e3f1be73fa0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test case demo</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
